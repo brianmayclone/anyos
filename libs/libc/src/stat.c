7a3033d3ad89c174a19c90812dafca75a6b00799
Christian Moeller <brianmayclone@googlemail.com>
2026-02-17 21:03:28 +0100

    FIXES to libc

--- libs/libc/src/stat.c
+++ libs/libc/src/stat.c
@@ -14,14 +14,22 @@ extern int _syscall(int num, int a1, int a2, int a3, int a4);
 #define SYS_MKDIR  90
 
 int stat(const char *path, struct stat *buf) {
-    unsigned int info[2]; /* type, size */
+    unsigned int info[6]; /* type, size, flags, uid, gid, mode */
     int ret = _syscall(SYS_STAT, (int)path, (int)info, 0, 0);
     if (ret < 0) { errno = -ret; return -1; }
     if (buf) {
         memset(buf, 0, sizeof(*buf));
-        buf->st_mode = (info[0] == 1) ? S_IFDIR | 0755 : S_IFREG | 0644;
+        unsigned int mode = info[5];
+        if (info[0] == 1)
+            buf->st_mode = S_IFDIR | (mode ? (mode & 0777) : 0755);
+        else if (info[0] == 2)
+            buf->st_mode = S_IFCHR | 0666;
+        else
+            buf->st_mode = S_IFREG | (mode ? (mode & 0777) : 0644);
         buf->st_size = info[1];
         buf->st_nlink = 1;
+        buf->st_uid = info[3];
+        buf->st_gid = info[4];
     }
     return 0;
 }
