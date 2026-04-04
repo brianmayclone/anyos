017eee14e624c9ee5260ad4fc0fe1b451bc132fc
strathmann <strathmann@swissperform.ch>
2026-04-04 22:26:25 +0200

    add test script for test262

---
+++ scripts/test262.sh
@@ -0,0 +1,7 @@
+for start in 1 1001 2001 3001 4001 5001 6001 7001 8001 9001 10001 11001 12001 13001 14001 15001 16001 17001 18001 19001 20001 21001 22001 23001 24001 25001 26001; do
+  echo "=== Block start=$start ==="
+  tools/test262-runner/target/x86_64-unknown-linux-gnu/release/test262-runner libs/libjs_tests/test262 --limit 1000 --start $start --verbose --timeout 2 2>&1 >/tmp/test262.$start.txt
+  echo -n "wait ....."
+  sleep 1
+  echo " ok"
+done
